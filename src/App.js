2a483763de8e22122e5a96c172880fd4ad056aa2
faran <faranmd1@gmail.com>
2023-01-08 23:01:07 +0530

    install dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import AllRoutes from "./routes/AllRoutes";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+  <Header/>
       <AllRoutes />
     </div>
   );
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -4,15 +4,19 @@ import Search from "../pages/Search/Search";
 import Movies from "../pages/Movies/Movies";
 import Shows from "../pages/Shows/Shows";
 import Player from "../pages/Player/Player";
+import SubscriptionPlans from "../components/SubscriptionPlans";
+import { Payment } from "../components/Payment";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/movies" element={<Movies />}></Route>
-      <Route path="/shows" element={<Shows />}></Route>
+      <Route path="/show" element={<Shows />}></Route>
       {/* <Route path="/myaha" element={<Myaha/>}></Route> */}
       <Route path="/search" element={<Search />}></Route>
+      <Route path="/subscribe" element={<SubscriptionPlans />}></Route>
+      <Route path="/payment" element={<Payment />}></Route>
       {/* <Route path="/movies/:id" element={<MoviePage/>}></Route> */}
       {/* <Route path="/shows/:id" element={<ShowPage/>}></Route> */}
       <Route path="/player" element={<Player/>}></Route>
